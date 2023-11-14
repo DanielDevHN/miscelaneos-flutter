9287ac4963df714e82964aa92e754d9f40998598
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-11-14 08:53:58 -0600

    Add new routes for location screens

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -53,5 +53,19 @@ final appRouter = GoRouter(
       path: '/biometrics',
       builder: (context, state) => const BiometricScreen(),
     ),
+
+    //! Location
+    GoRoute(
+      path: '/location',
+      builder: (context, state) => const LocationScreen(),
+    ),
+    GoRoute(
+      path: '/maps',
+      builder: (context, state) => const MapScreen(),
+    ),
+    GoRoute(
+      path: '/controlled-map',
+      builder: (context, state) => const ControlledMapScreen(),
+    ),
   ],
 );
