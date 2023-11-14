540e64d3eac0e83dacffaaf3b7fa8257bd01da94
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-11-14 08:55:16 -0600

    Add location screen in barrel file

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -2,6 +2,10 @@ export 'biometrics/biometric_screen.dart';
 
 export 'home/home_screen.dart';
 
+export 'location/controlled_map_screen.dart';
+export 'location/location_screen.dart';
+export 'location/map_screen.dart';
+
 export 'permissions/ask_location_screen.dart';
 export 'permissions/permissions_screen.dart';
 
