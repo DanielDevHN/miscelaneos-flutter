b9fbc4b3102949534648dc3888c51d3ebca79087
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-11-14 08:55:35 -0600

    Add location provider in barrel file

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -4,10 +4,11 @@ export 'compass/compass_provider.dart';
 
 export 'local_auth/local_auth_providers.dart';
 
+export 'location/use_location_provider.dart';
+
 export 'permissions/permissions_provider.dart';
 
 export 'pokemons/pokemon_list_provider.dart';
-
 export 'pokemons/pokemon_provider.dart';
 
 export 'sensors/accelerometer_providers.dart';
