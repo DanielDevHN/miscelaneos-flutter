47d51e52c29c93e94b3f601a9c0ffb88d46e9d4f
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-11-22 11:31:47 -0600

    Transform map screen in consumer widget

--- lib/presentation/screens/location/map_screen.dart
+++ lib/presentation/screens/location/map_screen.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-class MapScreen extends StatelessWidget {
+import 'package:miscelaneos/presentation/providers/providers.dart';
+
+class MapScreen extends ConsumerWidget {
   const MapScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final currentPositionAsync = ref.watch(userLocationProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Map Screen'),
+      ),
+      body: currentPositionAsync.when(
+          data: (data) => _MapView(initialLat: data.$1, initialLng: data.$2),
+          error: (error, stackTrace) => Center(child: Text('$error')),
+          loading: () => Center(
+                child: LoadingAnimationWidget.staggeredDotsWave(
+                    color: Color(Colors.blue.value), size: 70),
+              )),
+    );
+  }
+}
+
+class _MapView extends StatefulWidget {
+  final double initialLat;
+  final double initialLng;
+
+  const _MapView({required this.initialLat, required this.initialLng});
+
+  @override
+  State<_MapView> createState() => __MapViewState();
+}
+
+class __MapViewState extends State<_MapView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(widget.initialLat, widget.initialLng),
+          zoom: 12,
+        ),
+        myLocationEnabled: true,
+        zoomControlsEnabled: false,
+        onMapCreated: (GoogleMapController controller) {
+          // _controller.complete(controller);
+        },
       ),
     );
   }
