d82bb1f535f1e9a4c01c177b76b5e2db3357571b
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-11-25 11:27:39 -0600

    Add new shortcuts in providers barrel file

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,8 +1,10 @@
+export 'ads/admob_points_provider.dart';
+export 'ads/admob_providers.dart';
+export 'ads/show_ads_provider.dart';
+
 export 'app_state_provider.dart';
 
-export 'ads/admob_providers.dart';
-export 'ads/admob_points_provider.dart';
-export 'ads/show_ads_provider.dart';
+export 'background_tasks/background_tasks_provider.dart';
 
 export 'badge/badge_counter_provider.dart';
 
@@ -10,12 +12,13 @@ export 'compass/compass_provider.dart';
 
 export 'local_auth/local_auth_providers.dart';
 
+export 'location/map_controller_provider.dart';
 export 'location/user_location_provider.dart';
 export 'location/watch_location_provider.dart';
-export 'location/map_controller_provider.dart';
 
 export 'permissions/permissions_provider.dart';
 
+export 'pokemons/pokemon_db_provider.dart';
 export 'pokemons/pokemon_list_provider.dart';
 export 'pokemons/pokemon_provider.dart';
 
