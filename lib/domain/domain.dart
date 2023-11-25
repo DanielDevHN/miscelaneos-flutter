67fc8cba3f5df312b84995f1b01c6ef2ab9df57d
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-11-25 11:28:09 -0600

    Add new shortcuts in domain barrel file

--- lib/domain/domain.dart
+++ lib/domain/domain.dart
@@ -1,5 +1,7 @@
+export 'datasources/local_db_datasource.dart';
 export 'datasources/pokemons_datasource.dart';
 
 export 'entities/pokemon.dart';
 
+export 'repositories/local_db_repository.dart';
 export 'repositories/pokemons_repository.dart';
